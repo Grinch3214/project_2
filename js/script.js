eccfec7edfa4c46e73c7c3a547e9e4a39c2af021
Grinch3214 <78050226+Grinch3214@users.noreply.github.com>
2021-04-14 17:04:03 +0300

    JS fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 $(document). ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger, nav').toggleClass('active');
-		$('body').toggleClass('lock');
+		// $('body').toggleClass('lock'); без заданных id/адреса ссылкам при клике не убирает класс .lock
 	})
 });
+let menu=document.querySelector('nav');
+	menu.onclick=function(){
+		$('.header__burger, nav').toggleClass('active');
+	};
